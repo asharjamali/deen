ed12a1d69410fb186ad1ad4ca542a32fa490222c
asharjamali <asharjamali26@gmail.com>
2025-03-16 13:14:41 +0530

    done

--- Islamic-Website-main/React Project/src/data/navbarPages.js
+++ Islamic-Website-main/React Project/src/data/navbarPages.js
@@ -1,7 +1,7 @@
 const pages = [
     { name: 'Home', href: '/' },
     { name: 'Quran Kareem', href: '/quran' },
-    { name: 'Hijri Calender 'href: '/hijri-calendar' },
+    { name: 'Hijri Calender', href: '/hijri-calendar' },
     { name: 'Adhkar', href: '/adhkar' },
     { name: 'About', href: '/about' },
 ];
